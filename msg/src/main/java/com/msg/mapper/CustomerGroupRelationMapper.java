d1cdd2af32992500a5ffb860f9757d50f3dc9c57
kotori0815 <wd1995@126.com>
2018-03-19 07:08:16 +0800

    提交环境

---
+++ msg/src/main/java/com/msg/mapper/CustomerGroupRelationMapper.java
@@ -0,0 +1,18 @@
+package com.msg.mapper;
+
+
+import com.msg.entity.CustomerGroupRelation;
+
+public interface CustomerGroupRelationMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(CustomerGroupRelation record);
+
+    int insertSelective(CustomerGroupRelation record);
+
+    CustomerGroupRelation selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(CustomerGroupRelation record);
+
+    int updateByPrimaryKey(CustomerGroupRelation record);
+}
---
+++ msg/src/main/java/com/msg/service/UserService.java
@@ -0,0 +1,7 @@
+package com.msg.service;
+
+/**
+ * Created by wd199 on 2018/3/17.
+ */
+public interface UserService {
+}
